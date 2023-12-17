c067460c1b4c8fd863c436285e70ecd546bb84ce
Ozod <o.bukhorizoda@mail.ru>
2023-12-17 18:16:37 +0500

    created data json server

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,25 @@ import Header from "./components/header/Header";
 import Main from "./components/mainContent/Main";
 import Random from "./components/randomNumber/Random";
 import Name from "./components/name/Name";
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 export const ChangeName = createContext(null);
 function App() {
   const [value, setValue] = useState("");
+  const [datas, setDatas] = useState([]);
+
+  useEffect(() => {
+    const showCountries = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/countries");
+        const data = await response.json();
+        setDatas(data);
+        console.log(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    showCountries();
+  }, []);
   return (
     <>
       <ChangeName.Provider value={[value, setValue]}>
@@ -14,6 +29,11 @@ function App() {
         <Random />
         <Main />
         <Name />
+        {datas.map((data) => (
+          <div key={data.id}>
+            <img src={data.img} alt="Image" />
+          </div>
+        ))}
       </ChangeName.Provider>
     </>
   );
