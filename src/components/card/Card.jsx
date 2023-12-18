5dd54bd67659f98c1883982d8cbfa3ec3acf131f
Ozod <o.bukhorizoda@mail.ru>
2023-12-18 13:08:38 +0500

    handleView fn

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,13 +1,24 @@
 /* eslint-disable react/prop-types */
+import { useState } from "react";
 import styles from "./Card.module.css";
 
 export const Card = ({ data }) => {
+  const [details, setDetails] = useState(false);
+
+  const handleView = () => {
+    setDetails((prev) => !prev);
+  };
   return (
     <div className={styles.card}>
       <h3>{data.title}</h3>
       <img className={styles.card_img} src={data.img} alt={data.title} />
-      <p>{data.capital}</p>
-      <p>{data.population}</p>
+      {details && (
+        <>
+          <p>{data.capital}</p>
+          <p>{data.population}</p>
+        </>
+      )}
+      <button onClick={() => handleView()}>View more</button>
     </div>
   );
 };
